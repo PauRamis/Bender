c3b2f6f76851f599ca18e539a48180cc7b0d5bf1
Pau <pauramisnunez@gmail.com>
2023-02-23 19:04:43 +0100

    myMap

--- src/Bender.java
+++ src/Bender.java
@@ -1,14 +1,32 @@
 class Bender {
+    String mapa;
+
     // Constructor: ens passen el mapa en forma d'String
     public Bender(String mapa) {
-
+        this.mapa = mapa;
     }
     // Navegar fins a l'objectiu («$»).
-    // El valor retornat pel mètode consisteix en una cadena de
-    // caràcters on cada lletra pot tenir
-    // els valors «S», «N», «W» o «E»,
-    // segons la posició del robot a cada moment.
+    // El valor retornat pel mètode consisteix en una cadena dels valors «S», «N», «W» o «E».
+
     public String run() {
-        return "";
+        makeMap();
+        return findPath();
+    }
+
+    private void makeMap() {
+        String[] rows = this.mapa.split("\n");
+        int mapRows = rows.length;
+        int mapCols = rows[0].length();
+        char[][] myMap = new char[mapRows][mapCols];
+    }
+
+    private String findPath() {
+        String result = "";
+        Boolean onX = false;
+        while (!onX){
+            //robot();
+        }
+
+        return result;
     }
 }
